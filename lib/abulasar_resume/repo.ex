d3c0d9c2ba97ec30dc32f6ffc4e4d3f7bf10292b
AbulAsar S <abul.asar22@gmail.com>
2022-03-02 20:50:01 +0530

    configured database

---
+++ lib/abulasar_resume/repo.ex
@@ -0,0 +1,5 @@
+defmodule AbulasarResume.Repo do
+  use Ecto.Repo,
+    otp_app: :abulasar_resume,
+    adapter: Ecto.Adapters.Postgres
+end
--- mix.exs
+++ mix.exs
@@ -34,6 +34,9 @@ defmodule AbulasarResume.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.6.6"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.6"},
+      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
