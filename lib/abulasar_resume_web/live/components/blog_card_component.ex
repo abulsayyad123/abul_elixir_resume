b270e4aa86a12e0fd9e241d5ca6c122cf292372d
AbulAsar S <abul.asar22@gmail.com>
2022-09-25 20:15:24 +0530

    sucessfull POC of spinner

--- lib/abulasar_resume_web/live/components/blog_card_component.ex
+++ lib/abulasar_resume_web/live/components/blog_card_component.ex
@@ -4,7 +4,9 @@ defmodule AbulasarResumeWeb.BlogCardComponent do
   import AbulasarResumeWeb.BlogView
   import Phoenix.HTML
 
-
+  def mount(socket) do
+    {:ok, assign(socket, set_loading: false)}
+  end
   def render(assigns) do
     ~H"""
       <div class="item">
@@ -21,9 +23,33 @@ defmodule AbulasarResumeWeb.BlogCardComponent do
               <li><a href="#"><%= @blog["type"] %></a></li>
             </ul>
             <a><h4 class="blog-item-title" title={get_title(@blog)} ><%= @blog["title"] %></h4></a>
+            <i class="fa fa-plus" aria-hidden="true" phx-click="load_more_data" phx-target={@myself}></i>
+            <%= if @set_loading do %>
+              <i class="fa fa-spinner" aria-hidden="true" style="animation: rotation 2s infinite linear;"></i>
+            <% end %>
           </div>
         </div>
       </div>
     """
+  end
+
+  @impl true
+  def handle_event("load_more_data", _params, socket) do
+    send_update(__MODULE__, id: socket.assigns.id, action: :query_data)
+    socket = assign(socket, set_loading: true)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def update(%{action: :query_data}, socket) do
+    IO.puts "CALLED>>>>"
+    Process.sleep(3000)
+    socket = assign(socket, action: nil, set_loading: false)
+    {:ok, socket}
+  end
+
+  @impl true
+  def update(regular_assigns, socket) do
+    {:ok, assign(socket, regular_assigns)}
   end
 end
